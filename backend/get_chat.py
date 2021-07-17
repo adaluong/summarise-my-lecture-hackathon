5ff5d4b4db4adf83b3d33052a63b8b931bb8c5aa
adaluong <35322993+adaluong@users.noreply.github.com>
2021-07-18 02:26:02 +1000

    MAGIC

--- backend/get_chat.py
+++ backend/get_chat.py
@@ -1,6 +1,15 @@
 from chat_downloader import ChatDownloader
 
-url = 'https://www.youtube.com/watch?v=Vi231_PujYI'
-chat = ChatDownloader().get_chat(url)       # create a generator
-for message in chat:                        # iterate over messages
-    print(chat.format(message))             # print the formatted message
+if __name__ == "__main__":
+    url = 'https://www.youtube.com/watch?v=Vi231_PujYI'
+    url = 'https://www.youtube.com/watch?v=4WBbrxZguqk'
+    chat = ChatDownloader().get_chat(url)       # create a generator
+    for message in chat:                        # iterate over messages
+        print(chat.format(message))             # print the formatted message
+
+def id_to_chat(videoId: str) -> list:
+    chat = ChatDownloader().get_chat(f'https://www.youtube.com/watch?v={videoId}')
+    chat_list = []
+    for message in chat:
+        chat_list.append(chat.format(message))
+    return chat_list
---
+++ backend/magic.py
@@ -0,0 +1,50 @@
+# lecture repeats the question
+from fuzzywuzzy import fuzz
+import get_transcript
+import get_chat
+
+with open("transcript.txt") as f:
+    transcript = [line.strip() for line in f.readlines()]
+
+with open("chat.txt") as g:
+    chat = [line.strip() for line in g.readlines()]
+
+def magic(transcript, chat):
+    # keywords = ["ask", "questions", "ask", "question"]
+    for i in range(len(transcript) - 20):
+        line = transcript[i]
+        if 'question' in line:
+            question = transcript[i:i+3]
+            answer = transcript[i+3:i+20]
+
+            # print(f'Question: {" ".join(question)}\n')
+            # print(f'Answer: {" ".join(answer)}\n')
+        
+            for comment in chat:
+                potential_question = comment.split(" | ")[1]
+                if (fuzz.ratio(question, potential_question) > 48):
+                    print(f'{potential_question}')
+                    print(f'Question: {" ".join(question)}') 
+                    print(f'Answer: {" ".join(answer)}\n')
+
+    qna = []
+
+    return qna
+
+if __name__ == '__main__':
+
+    videoId = "4WBbrxZguqk"
+    transcript = get_transcript.id_to_transcript(videoId)
+    chat = get_chat.    id_to_chat(videoId)
+    
+    print(magic(transcript,chat))
+
+    marc = "what about alphabetical ordering of matt and mark"
+    chat = "What about alphabetical ordering of matt and marc"
+    # print(fuzz.ratio(marc, chat))
+
+    chat2 = "all of the students will do final exam at the same time?"
+
+    marc = "is it just returning one zero or negative one or does it return the magnitude of the difference"
+    chat = "Is it just returning 1/0/-1 or does it return the magnitude of the diffference?"
+    # print(fuzz.ratio(marc, chat))
