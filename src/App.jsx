e14bc851481c818a449b86bb1bc41a9044a5e1f1
Angeni Bai <angenibai@gmail.com>
2021-07-18 02:23:32 +1000

    frontend looks passable

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Result from './pages/Result';
 import Search from './pages/Search';
+import { Container, Navbar } from 'react-bootstrap';
 
 const App = () => {
   
   return (
     <Router>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Q&A Delivery Service</Navbar.Brand>
+        </Container>
+      </Navbar>
       <Route exact path='/' component={Search} />
       <Route exact path='/result/:videoId' component={Result} />
     </Router>
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import './Result.css';
 
 const Result = () => {
   const { videoId } = useParams();
-  const [transcript, setTranscript] = useState("");
+  const [qna, setQna] = useState([]);
+  const [videoName, setVideoName] = useState("");
 
   useEffect(() => {
     // this code is executed when the page is loaded/reloaded
     // fetch sends a query to our server with the id of the Youtube video
     // change the url as required
-    fetch(`/transcript?videoId=${videoId}`)
+    fetch(`/magic?id=${videoId}`)
       .then(r => r.json())
       .then(data => {
-        setTranscript(data.transcript);
+        setQna(data.qna);
+        setVideoName(data.name);
       });
   }, []);
 
   return (
     <div className="Result">
       <h1>
-        Video ID: {videoId}
+        {videoName}
       </h1>
-      <h3>
-        Transcript
-      </h3>
-      <p>
-        {transcript}
-      </p>
-    </div>
+      <div className="qna">
+        {qna.map((element, idx) => (
+          <Card className="qnaCard" id={idx}>
+            <Card.Header className="qnaQuestion">
+              <strong>Q: </strong>{element.question}
+            </Card.Header>
+            <Card.Text className="qnaAnswer">
+              <strong>A: </strong>{element.answer}
+            </Card.Text>
+          </Card>
+        ))}
+      </div>
+   </div>
   );
 }
 
